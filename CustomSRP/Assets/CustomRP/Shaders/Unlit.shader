e5d95bd526828a970391c107f132a8ceefa5e000
Gaocheng <rangaocheng@outlook.com>
2025-08-19 18:21:30 +0800

    Blend Modes; Not Writing Depth; Texturing

--- CustomSRP/Assets/CustomRP/Shaders/Unlit.shader
+++ CustomSRP/Assets/CustomRP/Shaders/Unlit.shader
@@ -2,14 +2,29 @@ Shader "Custom RP/Unlit"
 {
     Properties
     {
+        // '{}':  It was used to control the texture settings long ago, but should still be included today to prevent weird errors in some cases.
+        _BaseMap("Texture", 2D) = "white" {}
         // The property name must be followed by a string for use in the inspector and a Color type identifier, as if providing arguments to a method.
         _BaseColor("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        [Enum(UnityEngine.Rendering.BlendMode)]
+
+        // The "source" refers to the color to be drawn, and the "destination" refers to the current color of the pixel.
+        _SrcBlend("Src Blend", Float) = 1
+        [Enum(UnityEngine.Rendering.BlendMode)]
+        _DstBlend("Dst Blend", Float) = 0
+
+        [Enum(Off, 0, On, 1)]
+        _ZWrite("Z Write", Float) = 1
     }
     SubShader
     {
         Pass
         {
+            Blend [_SrcBlend] [_DstBlend]
+            ZWrite [_ZWrite]
+
             HLSLPROGRAM
+            // This command will cause Unity to generate two variants of this Shader, one supporting GPU Instancing and the other not.
             #pragma multi_compile_instancing
             #pragma vertex UnlitPassVertex
             #pragma fragment UnlitPassFragment
--- CustomSRP/Assets/CustomRP/Shaders/UnlitPass.hlsl
+++ CustomSRP/Assets/CustomRP/Shaders/UnlitPass.hlsl
@@ -28,8 +28,14 @@ cbuffer UnityPerMaterial
 // float4 _BaseColor;
 // CBUFFER_END
 
+TEXTURE2D(_BaseMap);
+SAMPLER(sampler_BaseMap);
+
 // To use GPU Instancing, each instance data needs to be constructed as an array, and each instance data is wrapped using UNITY_INSTANCING_BUFFER_START(END)
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    // Unity makes the tiling and offset of the texture available via a float4 that has the same name as the texture property but with _ST appended, 
+    // which stands for scale and translation or something like that.
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
     // The definition format of _BaseColor in an array
     UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
@@ -37,13 +43,15 @@ UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 struct Attributes
 {
     float3 positionOS : POSITION;
+    float2 baseUV : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings
 {
     float4 positionCS : SV_POSITION;
-	UNITY_VERTEX_INPUT_INSTANCE_ID
+    float2 baseUV : VAR_BASE_UV;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 
@@ -60,6 +68,9 @@ Varyings UnlitPassVertex(Attributes input)
     UNITY_TRANSFER_INSTANCE_ID(input, output);
     float3 positionWS = TransformObjectToWorld(input.positionOS);
     output.positionCS = TransformWorldToHClip(positionWS);
+    
+    float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    output.baseUV = input.baseUV * baseST.xy + baseST.zw;
     return output;
 }
 
@@ -67,8 +78,10 @@ float4 UnlitPassFragment(Varyings input) : SV_TARGET
 {
     // Extract the ID of the instance from the input and store it in the global static variable on which other instantiated macros depend
     UNITY_SETUP_INSTANCE_ID(input);
+    float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
     // Obtain the data for each instance
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);;
+    float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    return baseMap * baseColor;
 }
 
 #endif
